f704431132b568164e7289b840c2e3bb3e53db5d
fuji066 <efu066@gmail.com>
2022-01-16 22:01:17 +0900

    FirstCommit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rbody;
+    float axisH = 0.0f;
+    float axisV = 0.0f;
+    public float speed = 3.0f;
+
+    public float jump = 9.0f;
+    public LayerMask groundLayer;
+
+    public static string gameState = "playing";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = this.GetComponent<Rigidbody2D>();
+        gameState = "playing";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameState != "playing")
+        {
+            return;
+        }
+
+        axisH = Input.GetAxisRaw("Horizontal");
+        axisV = Input.GetAxisRaw("Vertical");
+        if(axisH > 0.0f)
+        {
+            transform.localScale = new Vector2(1,1);
+        }
+        else if(axisH < 0.0f)
+        {
+            transform.localScale = new Vector2(-1,1);
+        }
+        
+        if(axisV > 0.0f)
+        {
+            transform.localScale = new Vector2(1,1);
+        }
+        else if(axisV < 0.0f)
+        {
+            transform.localScale = new Vector2(1,-1);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(gameState != "playing")
+        {
+            return;
+        }
+        rbody.velocity = new Vector2(speed * axisH,speed * axisV);
+
+    }
+    public void Jump()
+    {
+        rbody.velocity = new Vector2(speed * axisH,+100);
+        Debug.Log("JumpButton");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Goal")
+        {
+            Goal();
+        }
+        else if(collision.gameObject.tag == "Dead")
+        {
+            GameOver();
+        }
+    }
+
+    public void Goal()
+    {
+        gameState = "gameclear";
+        GameStop();
+    }
+
+    public void GameOver()
+    {
+        gameState = "gameover";
+        GameStop();
+        //GameOver‰‰o
+        GetComponent<BoxCollider2D>().enabled = false;
+        rbody.AddForce(new Vector2(0,5),ForceMode2D.Impulse);
+    }
+    void GameStop()
+    {
+        Rigidbody2D rbody = GetComponent<Rigidbody2D>();
+        rbody.velocity = new Vector2(0,0);
+    }
+}
